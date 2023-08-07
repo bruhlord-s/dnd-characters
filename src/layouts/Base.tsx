acdfce62e553465b1def9473e27a8ac863733328
Treize Konstantin <treize.konstantin@yandex.ru>
2023-08-07 18:12:34 +0600

    project setup, added some layouts

---
+++ src/layouts/Base.tsx
@@ -0,0 +1,11 @@
+import { FC, PropsWithChildren } from "react";
+import styles from "../assets/css/baseLayout.module.css";
+
+// base styles, variables
+import "../assets/css/base.css";
+
+const BaseLayout: FC<PropsWithChildren> = ({ children }) => {
+  return <div className={styles.baseLayout}>{children}</div>;
+};
+
+export default BaseLayout;
---
+++ src/layouts/UnderConstruction.tsx
@@ -0,0 +1,15 @@
+import { FC, PropsWithChildren } from "react";
+import styles from "../assets/css/underConstructionLayout.module.css";
+
+const UnderConstructionLayout: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <div className={styles.underConstructionLayout}>
+      <div className={styles.underConstructionLayout__warning}>
+        App is under construction
+      </div>
+      <div className={styles.underConstructionLayout__content}>{children}</div>
+    </div>
+  );
+};
+
+export default UnderConstructionLayout;
